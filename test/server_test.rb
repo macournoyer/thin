4f20c1606c9d80e1f807e6c60b49c97dbe0298ca
macournoyer <macournoyer@ab7993de-5426-0410-a80b-baa00616f331>
2007-11-27 04:04:08 +0000

    Test a couple more paths in the server class
    git-svn-id: http://code.macournoyer.com/svn/thin/trunk@342 ab7993de-5426-0410-a80b-baa00616f331

--- test/server_test.rb
+++ test/server_test.rb
@@ -10,37 +10,34 @@ end
 class ServerTest < Test::Unit::TestCase
   def setup
     @handler = TestHandler.new
-    TCPServer.stubs(:new)
+    
+    @socket = stub_everything
+    TCPServer.stubs(:new).returns(@socket)
     
     @server = Thin::Server.new('0.0.0.0', 3000, @handler)
     @server.logger = Logger.new(nil)
-    
-    @socket = stub_everything
-    @server.instance_variable_set :@socket, @socket
   end
   
-  def test_successful_run
-    client = StringIO.new('no-empty')
-    @socket.stubs(:accept).returns(client)
-    @socket.stubs(:closed?).returns(false).then.returns(true)
-    
-    client.expects(:readpartial).returns(<<EOS)
-GET / HTTP/1.1
-Host: localhost:3000
-EOS
-    client.stubs(:peeraddr).returns([])
-    client.expects(:close)
-    
+  def test_ok
+    request "GET / HTTP/1.1\n\rHost: localhost:3000"
     @server.start
     
-    client.rewind
-    assert_equal <<EOS.chomp, client.read.delete("\r")
-HTTP/1.1 200 OK
-Content-Length: 9
-Connection: close
-
-test body
-EOS
+    assert_response 'test body', :status => 200
+  end
+  
+  def test_bad_request
+    request "FUCKED / STUFF/1.1\n\rnononon"
+    @server.start
+    
+    assert_response 'Bad request', :status => 400
+  end
+  
+  def test_not_found
+    @handler.stubs(:process).returns(false)
+    request "GET / HTTP/1.1\n\rHost: localhost:3000"
+    @server.start
+    
+    assert_response 'Page not found', :status => 404    
   end
   
   def test_stop
@@ -48,4 +45,23 @@ EOS
     @socket.expects(:close)
     @server.stop
   end
+  
+  private
+    def request(body)
+      @client = StringIO.new('not-empty')
+      @socket.stubs(:accept).returns(@client)
+      @socket.stubs(:closed?).returns(false).then.returns(true)
+      
+      @client.expects(:readpartial).returns(body)
+      @client.stubs(:peeraddr).returns([])
+      @client.expects(:close)
+    end
+      
+    def assert_response(body, options={})
+      status = options.delete(:status) || 200
+      @client.rewind
+      response = @client.read
+      assert_match "HTTP/1.1 #{status} #{Thin::HTTP_STATUS_CODES[status]}", response
+      assert_match body, response
+    end
 end
