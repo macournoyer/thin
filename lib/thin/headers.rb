e1a5f9bf30d2ff96e20361b24582c45bf0480ed7
macournoyer <macournoyer@ab7993de-5426-0410-a80b-baa00616f331>
2007-12-01 20:08:28 +0000

    Allow overriding headers values
    git-svn-id: http://code.macournoyer.com/svn/thin/trunk@354 ab7993de-5426-0410-a80b-baa00616f331

--- lib/thin/headers.rb
+++ lib/thin/headers.rb
@@ -1,16 +1,19 @@
 module Thin
   # Acts like a Hash, but allows duplicated keys
-  # Class largely based on Mongrel::CGIWrapper
-  # http://mongrel.rubyforge.org/ by Zed Shaw
   class Headers
+    @@allowed_duplicates = %w(Set-Cookie Set-Cookie2 Warning WWW-Authenticate)
+    
     def initialize
       @sent = {}
       @items = []
-      @allowed_duplicates = %w(Set-Cookie Set-Cookie2 Warning WWW-Authenticate)
     end
 
     def []=(key, value)
-      if ! @sent.has_key?(key) || @allowed_duplicates.include?(key)
+      if @sent.has_key?(key) && !@@allowed_duplicates.include?(key)
+        # If we don't allow duplicate for that field
+        # we overwrite the one that is already there
+        @items.assoc(key)[1] = value
+      else
         @sent[key] = true
         @items << [key, value]
       end
