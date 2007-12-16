ca6bae9c255554e4ca8e04f15f594ac6be3fcddb
macournoyer <macournoyer@ab7993de-5426-0410-a80b-baa00616f331>
2007-12-16 23:28:13 +0000

    Update benchmarks and web site
    git-svn-id: http://code.macournoyer.com/svn/thin/trunk@401 ab7993de-5426-0410-a80b-baa00616f331

--- site/thin.rb
+++ site/thin.rb
@@ -42,22 +42,38 @@ class Thin < Atchoum::Website
   
   def index_page
     h2 'What'
-    p do
-      <<-EOS        
-        Thin is a web server written entirely in Ruby in the #{acronym('simplest', :title => 'under 2 KLOC')} way possible.
-        It does as little as possible to serve your #{a 'Rails', :href => 'http://rubyonrails.org', :title => 'Ruby on Rails web framework'} application,
-        which makes it one of the fastest Rails server out there.
-      EOS
+    p 'Thin is a Ruby web server that glues togeter 3 of the best Ruby libraries in web history:'
+    ul do
+      li do
+        text "the #{a 'Mongrel parser', :href => 'http://www.zedshaw.com/tips/ragel_state_charts.html'}"
+        text ", the root of #{a 'Mongrel', :href => 'http://mongrel.rubyforge.org/'} speed and security"
+      end
+      li do
+        a "Event Machine", :href => 'http://rubyeventmachine.com/'
+        text ", a network I/O library with extremely high scalability, performance and stability"
+      end
+      li do
+        a "Rack", :href => 'http://rack.rubyforge.org/'
+        text ", a minimal interface between webservers and Ruby frameworks"
+      end
     end
+    p <<-EOS
+      Which makes it, with all humility, the most secure, stable, fast and extensible Ruby web server
+      bundled in an easy to use gem for your own plesure.
+    EOS
     
     h2 'Why'
-    ul do
-      li { "10 % faster than #{a 'Mongrel', :href => 'http://mongrel.rubyforge.org/', :title => 'Mongrel web server'}" }
-      li { "5 MB less memory than Mongrel" }
-      li { "No C extension, 100% pure and beautiful Ruby" }
-      li { "No dependency" }
-      li { "Built-in cluster monitoring" }
-      li { "0 downtime cluster restart" }
+    div.graph do
+      h3 'Request / seconds comparison'
+      img :alt => 'Chart', :title => 'Benchmarks', :src => chart_url([
+        ['WEBrick',     [297.37, 296.65, 297.16]],
+        ['Mongrel',     [556.67, 622.90, 428.23]],
+        ['Evented M.',  [517.97, 657.89, 656.17]],
+        ['Thin',        [719.53, 782.11, 776.40]]
+      ], :min => 200, :scale => 6.5, :size => '350x150', :width => 16,
+         :colors => %w(000000 666666 cccccc),
+         :legends => ['1 c req.', '10 c req.', '100 c req.'])
+      em 'c req. = Concurrency Level (number of simultaneous requests)'
     end
     
     h2 'How'
@@ -65,5 +81,21 @@ class Thin < Atchoum::Website
 
     p 'Go to your Rails app directory and run:'
     pre 'thin start'
+  end
+  
+  def chart_url(data, options={})
+    size   = options[:size]  || '250x150'
+    width  = options[:width] || 50
+    min    = options[:min]   || 0
+    scale  = options[:scale] || 1.0
+    colors = options[:colors] || '000000'
+    legends = options[:legends]
+    
+    data_matrix  = data.collect { |d| Array(d[1]) }
+    chart_matrix = data_matrix[0].zip(*data_matrix[1..-1]) # transpose the matrix
+    chart_data   = chart_matrix.collect { |c| c.collect { |d| (d - min) / scale }.join(',') }.join('|')
+    labels       = data.collect{|d|d[0]}.join('|')
+    
+    "http://chart.apis.google.com/chart?cht=bvg&chd=t:#{chart_data}&chbh=#{width}&chs=#{size}&chl=#{labels}&chco=#{colors.join(',')}&chdl=#{legends.join('|')}"
   end
 end
