a6b4cae783e97e083a63221b4fa4878c4b5f0198
macournoyer <macournoyer@gmail.com>
2008-01-20 23:36:32 -0500

    Alter response headers to output directly to a string.

--- spec/headers_spec.rb
+++ spec/headers_spec.rb
@@ -9,20 +9,14 @@ describe Headers do
     @headers['Set-Cookie'] = 'twice'
     @headers['Set-Cookie'] = 'is cooler the once'
     
-    @headers.size.should == 2
+    @headers.to_s.should == "Set-Cookie: twice\r\nSet-Cookie: is cooler the once\r\n"
   end
   
   it 'should overwrite value on non duplicate fields' do
     @headers['Host'] = 'this is unique'
     @headers['Host'] = 'so is this'
-    @headers['Host'] = 'so this will overwrite ^'
 
-    @headers['Host'].should == 'so this will overwrite ^'
-  end
-  
-  it 'should return first header value' do
-    @headers['Set-Cookie'] = 'hi'
-    @headers['Set-Cookie'].should == 'hi'
+    @headers.to_s.should == "Host: this is unique\r\n"
   end
   
   it 'should output to string' do
