fe618685efcb35f251865703a8028376008a5b10
macournoyer <macournoyer@gmail.com>
2008-01-28 21:00:30 -0500

    Fix benchmark script

--- benchmark/simple.rb
+++ benchmark/simple.rb
@@ -3,12 +3,13 @@
 #
 # Run with:
 #
-#  ruby simple.rb [num of request]
+#  ruby simple.rb [num of request] [print|graph] [concurrency levels]
 #
 require File.dirname(__FILE__) + '/../lib/thin'
 require File.dirname(__FILE__) + '/utils'
 
-request = (ARGV[0] || 1000).to_i # Number of request to send (ab -n option)
+request     = (ARGV[0] || 1000).to_i # Number of request to send (ab -n option)
 output_type = (ARGV[1] || 'print')
+levels      = eval(ARGV[2] || '[1, 10, 100]').to_a
 
-benchmark output_type, %w(WEBrick Mongrel EMongrel Thin), request, [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+benchmark output_type, %w(WEBrick Mongrel EMongrel Thin), request, levels
