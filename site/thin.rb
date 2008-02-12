43c8cf8730fa24a4d5d2bdc0aa769d0e54f0f335
macournoyer <macournoyer@gmail.com>
2008-02-12 12:36:29 -0500

    Adding moonitor.org to the users page.

--- site/thin.rb
+++ site/thin.rb
@@ -198,6 +198,7 @@ class Thin < Atchoum::Website
       li { a "RaPlanet", :href => 'http://planet.zhekov.net/' }
       li { a "Ninja Hideout blog", :href => 'http://blog.ninjahideout.com/' }
       li { a "blog.hoodow.de", :href => 'http://blog.hoodow.de/articles/2008/02/09/thin' }
+      li { a "moonitor.org", :href => 'http://moonitor.org' }
     end
     
     p { "If you'd like to have your site listed here, #{a 'drop me an email', :href => 'mailto:macournoyer@gmail.com'}" }
