17902e38bcbb4ba6198d50a0072653362d8763f9
macournoyer <macournoyer@gmail.com>
2008-02-25 10:12:05 -0500

    truncate code on usage page

--- site/thin.rb
+++ site/thin.rb
@@ -119,18 +119,18 @@ class Thin < Atchoum::Website
     pre <<-EOS.gsub(/^\s{6}/, '')
       app = proc do |env|
         [
-          200,                      # Status code
-          {                         # Response headers
+          200,          # Status code
+          {             # Response headers
             'Content-Type' => 'text/html',
             'Content-Length' => '2',
           },
-          ['hi']                    # Response body
+          ['hi']        # Response body
         ]
       end
       
-      # You can install Rack middlewares to do some
-      # crazy stuff like logging, filtering, auth
-      # or build your own.
+      # You can install Rack middlewares
+      # to do some crazy stuff like logging,
+      # filtering, auth or build your own.
       use Rack::CommonLogger
       
       run app
