3b7357dcbc165ac546fa51b0f14995a1d37e4912
macournoyer <macournoyer@gmail.com>
2009-01-16 16:06:30 -0500

    Fix error when passing no_epoll option to a cluster.

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -4,7 +4,7 @@ describe Command do
   before do
     Command.script = 'thin'
     @command = Command.new(:start, :port => 3000, :daemonize => true, :log => 'hi.log',
-                           :require => %w(rubygems thin))
+                           :require => %w(rubygems thin), :no_epoll => true)
   end
   
   it 'should shellify command' do
@@ -16,5 +16,10 @@ describe Command do
   it 'should shellify Array argument to multiple parameters' do
     out = @command.shellify
     out.should include('--require="rubygems"', '--require="thin"')
+  end
+
+  it 'should convert _ to - in option name' do
+    out = @command.shellify
+    out.should include('--no-epoll')
   end
 end
