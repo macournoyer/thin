c4e9a08228cd310836ffb1fd94d5c6ef05c65cab
macournoyer <macournoyer@gmail.com>
2011-11-14 23:25:52 -0500

    More options.

--- lib/thin/server.rb
+++ lib/thin/server.rb
@@ -1,41 +1,47 @@
 require "preforker"
 require "eventmachine"
+require 'socket'
 
 require "thin/connection"
 require "thin/system"
 
 module Thin
   class Server
-    attr_accessor :app, :address, :port, :backlog, :worker_timeout, :pid_path, :log_path
+    attr_accessor :app, :address, :port, :backlog, :worker_timeout, :pid_path, :log_path, :use_epoll, :maximum_connections
     
     def initialize(app, address="0.0.0.0", port=3000)
       @app = app
       @address = address
       @port = port
-      @backlog = 50
+      @backlog = 1024
       @worker_timeout = 5
       @pid_path = "./thin.pid"
       @log_path = "./thin.log"
+      @use_epoll = true
+      @maximum_connections = 1024
     end
     
     def start(workers=nil)
       socket = TCPServer.new(@address, @port)
       socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+      socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true)
       socket.listen(@backlog)
 
       trap("EXIT") { socket.close }
       
-      if workers.nil?
-        workers = System.processor_count
-        puts "Detected #{workers} processor(s)"
-      end
+      workers = System.processor_count if workers.nil?
+      
+      EM.epoll if @use_epoll
+      @maximum_connections = EM.set_descriptor_table_size(@maximum_connections)
+      puts "Maximum connections set to #{@maximum_connections} per worker"
       
       puts "Starting #{workers} worker(s) ..."
-      Preforker.new(:workers => workers,
+      prefork = Preforker.new(:workers => workers,
                     :app_name => "Thin",
                     :timeout => @worker_timeout,
                     :pid_path => pid_path,
                     :logger => Logger.new(@log_path)) do |master|
+        
         EM.run do
           EM.add_periodic_timer(4) do
             EM.stop_event_loop unless master.wants_me_alive?
@@ -43,7 +49,10 @@ module Thin
           
           EM.attach_server(socket, Connection) { |c| c.server = self }
         end
-      end.run
+      end
+      
+      puts "Listening on #{@address}:#{@port}, CTRL+C to stop"
+      prefork.run
     end
   end
 end
