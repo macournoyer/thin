55ad4f2a5fdb8a0b904b974853d6ec25b54ebb5e
Jonas Pfenniger <jonas@pfenniger.name>
2012-03-24 20:58:05 +0000

    Adjust http_parser headers to thin

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source :rubygems
 
 gemspec
 
+group :development do
+  gem 'pry'
+end
+
 group :test do
   gem 'rake' # for travis-ci.org
   gem 'rspec', '~> 1.2.9'
--- lib/thin/request.rb
+++ lib/thin/request.rb
@@ -13,23 +13,38 @@ module Thin
     MAX_BODY          = 1024 * (80 + 32)
     BODY_TMPFILE      = 'thin-body'.freeze
     MAX_HEADER        = 1024 * (80 + 32)
-    
+
     INITIAL_BODY      = ''
     # Force external_encoding of request's body to ASCII_8BIT
     INITIAL_BODY.encode!(Encoding::ASCII_8BIT) if INITIAL_BODY.respond_to?(:encode!)
-    
+
     # Freeze some HTTP header names & values
-    SERVER_SOFTWARE   = 'SERVER_SOFTWARE'.freeze
-    SERVER_NAME       = 'SERVER_NAME'.freeze
-    LOCALHOST         = 'localhost'.freeze
-    HTTP_VERSION      = 'HTTP_VERSION'.freeze
+    %w[
+      CONTENT_LENGTH
+      GATEWAY_INTERFACE
+      HTTP_HOST
+      HTTP_VERSION
+      PATH_INFO
+      QUERY_STRING
+      REMOTE_ADDR
+      REQUEST_METHOD
+      REQUEST_PATH
+      REQUEST_URI
+      SCRIPT_NAME
+      SERVER_NAME
+      SERVER_PORT
+      SERVER_PROTOCOL
+      SERVER_SOFTWARE
+    ].each do |const|
+      const_set(const, const.freeze)
+    end
+
     HTTP_1_0          = 'HTTP/1.0'.freeze
-    REMOTE_ADDR       = 'REMOTE_ADDR'.freeze
-    CONTENT_LENGTH    = 'CONTENT_LENGTH'.freeze
     CONNECTION        = 'HTTP_CONNECTION'.freeze
+    LOCALHOST         = 'localhost'.freeze
     KEEP_ALIVE_REGEXP = /\bkeep-alive\b/i.freeze
     CLOSE_REGEXP      = /\bclose\b/i.freeze
-    
+
     # Freeze some Rack header names
     RACK_INPUT        = 'rack.input'.freeze
     RACK_VERSION      = 'rack.version'.freeze
@@ -37,6 +52,7 @@ module Thin
     RACK_MULTITHREAD  = 'rack.multithread'.freeze
     RACK_MULTIPROCESS = 'rack.multiprocess'.freeze
     RACK_RUN_ONCE     = 'rack.run_once'.freeze
+    RACK_URL_SCHEME   = 'rack.url_scheme'.freeze
     ASYNC_CALLBACK    = 'async.callback'.freeze
     ASYNC_CLOSE       = 'async.close'.freeze
 
@@ -50,7 +66,7 @@ module Thin
     attr_reader :body
 
     def initialize
-      @parser   = HTTP::RequestParser.new
+      @parser   = HTTP::Parser.new
       @parser.on_headers_complete = method(:on_headers_complete)
       @parser.on_body = method(:on_body)
       @parser.on_message_begin = method(:on_message_begin)
@@ -71,27 +87,54 @@ module Thin
 
         RACK_MULTITHREAD  => false,
         RACK_MULTIPROCESS => false,
-        RACK_RUN_ONCE     => false
+        RACK_RUN_ONCE     => false,
+        CONNECTION        => ''
       }
     end
 
     protected
 
     def on_headers_complete(headers)
-      # TODO: adjust the keys there
-      headers.each_pair do |key, value|
-        @env["HTTP_#{key.upcase}"] = value
+      if (content_length = headers.delete('Content-Length'))
+        @env[CONTENT_LENGTH] = content_length
+
+        # Transfert to a tempfile if body is very big
+        move_body_to_tempfile if content_length.to_i > MAX_BODY
       end
-      # TODO: http_version ?? method ? query_string ?
 
-      content_length = headers['Content-Length'].to_i
+      @env[GATEWAY_INTERFACE] = 'CGI/1.2'
+      @env[HTTP_VERSION] = @env[SERVER_PROTOCOL] = "HTTP/#{@parser.http_version.join('.')}"
+      @env[REQUEST_METHOD] = @parser.http_method
+      @env[QUERY_STRING] = @parser.query_string
+      @env[PATH_INFO] = @env[REQUEST_PATH] = @parser.request_path
+      @env[REQUEST_URI] = @parser.request_url
+      @env[SCRIPT_NAME] = ''
 
-      # Transfert to a tempfile if body is very big
-      move_body_to_tempfile if content_length > MAX_BODY
+      @env[HTTP_HOST] = 'localhost:9292' # TODO
+      @env[SERVER_PORT] = '9292' # TODO
+      @env[RACK_URL_SCHEME] = 'http' # TODO
+
+=begin
+      keep_alive?
+      upgrade?
+      status_code
+      request_url
+      request_path
+      fragment
+      upgrade_data
+      header_value_type = :mixed | :arrays | :strings
+      reset!
+=end
+
+      # Convert back all the other headers to rack-compatible
+      # TODO: verify if header_value_type == :mixed is the right-one
+      headers.each_pair do |key, value|
+        @env["HTTP_#{key.gsub('-', '_').upcase}"] = value
+      end
     end
 
-    def on_body(*a)
-      p [:body, *a]
+    def on_body(chunk)
+      @body << chunk
     end
 
     def on_message_begin
@@ -112,9 +155,10 @@ module Thin
     # Returns +true+ if the parsing is complete.
     def parse(data)
       @parser << data
-      # TODO: re-raise with Thin's own error
-      # TODO: raise InvalidRequest, 'Header longer than allowed' if @data.size > MAX_HEADER
       @finished
+    rescue HTTP::Parser::Error => ex
+      # re-raise as thin's error
+      raise InvalidRequest, ex
     end
 
     # +true+ if headers and body are finished parsing
