31f73886a48eef530693ddc68c729b091abcf5f2
macournoyer <macournoyer@gmail.com>
2012-08-06 10:33:49 -0400

    Update sample config file w/ more options.

--- examples/thin.conf.rb
+++ examples/thin.conf.rb
@@ -1,23 +1,33 @@
 # Event backend options
 worker_connections 1024
-# Disable epoll or kqueue will fallback to select.
+# Disabling epoll or kqueue will fallback to select.
 # use_epoll false
 # use_kqueue false
 
+# Threading
+# For slow apps, you can enable the use of threads.
+# If you're using this in Rails, make sure to call config.threadsafe!
+threaded true # Call the app in a thread.
+thread_pool_size 20
+
 # Worker options
-# worker_processes 0 # runs in a single process w/ limited features
+# worker_processes 0 # runs in a single process w/ limited features.
 worker_processes 4
 timeout 30 # seconds
+
+# Preload the app (the .ru file) before forking to workers.
+# Enable with copy-on-write garbage collection for better memory usage.
+preload_app true
 
 # Logging
 log_path "./thin.log"
 pid_path "./thin.pid"
 
 # Listeners
-listen 3000, :backlog => 128, :tcp_no_delay => true
-listen "0.0.0.0:8080", :protocol => :http
-listen "[::]:8080" # IPv6
-listen "/tmp/thin.sock"
+listen 3000, :backlog => 1024, :tcp_no_delay => true
+listen "0.0.0.0:8080"
+listen "[::]:8081" # IPv6
+listen "/tmp/thin.sock" # UNIX domain socket
 
 # Custom protocol
 class Echo < EventMachine::Connection
