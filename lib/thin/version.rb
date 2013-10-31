11346bb433958d71e6e7c7c4405e2a82a7f63d04
macournoyer <macournoyer@gmail.com>
2013-10-31 10:43:46 -0400

    Downgrade Rack required version back to 1.0 to work w/ prior Rails versions.
    Also name version 1.6.1 Death Proof.

--- lib/thin/version.rb
+++ lib/thin/version.rb
@@ -10,7 +10,7 @@ module Thin
     
     STRING   = [MAJOR, MINOR, TINY].join('.')
     
-    CODENAME = "???".freeze
+    CODENAME = "Death Proof".freeze
     
     RACK     = [1, 0].freeze # Rack protocol version
   end
