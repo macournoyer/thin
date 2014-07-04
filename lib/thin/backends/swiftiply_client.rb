2565cc885ceb70579c0d5bbaf3cba9ade8dc2a32
patriot <nazarok.oi@gmail.com>
2014-07-04 21:09:31 +0300

    refactoring

--- lib/thin/backends/swiftiply_client.rb
+++ lib/thin/backends/swiftiply_client.rb
@@ -3,10 +3,10 @@ module Thin
     # Backend to act as a Swiftiply client (http://swiftiply.swiftcore.org).
     class SwiftiplyClient < Base
       attr_accessor :key
-      
+
       attr_accessor :host, :port
-      
-      def initialize(host, port, options={})
+
+      def initialize(host, port, options = {})
         @host = host
         @port = port.to_i
         @key  = options[:swiftiply].to_s
@@ -26,31 +26,31 @@ module Thin
       def to_s
         "#{@host}:#{@port} swiftiply"
       end
-    end    
+    end
   end
 
   class SwiftiplyConnection < Connection
     def connection_completed
       send_data swiftiply_handshake(@backend.key)
     end
-    
+
     def persistent?
       true
     end
-    
+
     def unbind
       super
       EventMachine.add_timer(rand(2)) { reconnect(@backend.host, @backend.port) } if @backend.running?
     end
-    
+
     protected
       def swiftiply_handshake(key)
-        'swiftclient' << host_ip.collect { |x| sprintf('%02x', x.to_i)}.join << sprintf('%04x', @backend.port) << sprintf('%02x', key.length) << key
+        'swiftclient' << host_ip.collect { |x| sprintf('%02x', x.to_i) }.join << sprintf('%04x', @backend.port) << sprintf('%02x', key.length) << key
       end
-      
+
       # For some reason Swiftiply request the current host
       def host_ip
-        Socket.gethostbyname(@backend.host)[3].unpack('CCCC') rescue [0,0,0,0]
+        Socket.gethostbyname(@backend.host)[3].unpack('CCCC') rescue [0, 0, 0, 0]
       end
   end
 end
