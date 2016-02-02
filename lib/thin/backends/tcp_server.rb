7bb1c28f811d89feb2538f36afae64312db12640
Mark Eschbach <meschbach@gmail.com>
2016-02-02 12:49:49 -0800

    Use the correct port when 0 is passed

--- lib/thin/backends/tcp_server.rb
+++ lib/thin/backends/tcp_server.rb
@@ -4,23 +4,28 @@ module Thin
     class TcpServer < Base
       # Address and port on which the server is listening for connections.
       attr_accessor :host, :port
-      
+
       def initialize(host, port)
         @host = host
         @port = port
         super()
       end
-      
+
       # Connect the server
       def connect
         @signature = EventMachine.start_server(@host, @port, Connection, &method(:initialize_connection))
+        binary_name = EventMachine.get_sockname( @signature )
+        port_name = Socket.unpack_sockaddr_in( binary_name )
+        @port = port_name[0]
+        @host = port_name[1]
+        @signature
       end
-      
+
       # Stops the server
       def disconnect
         EventMachine.stop_server(@signature)
       end
-            
+
       def to_s
         "#{@host}:#{@port}"
       end
