85b956cf1cf377e3e53911cd4e717f7392f7f818
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-03-31 23:58:22 +1300

    Bump patch version.

--- lib/thin/version.rb
+++ lib/thin/version.rb
@@ -6,11 +6,11 @@ module Thin
   module VERSION #:nodoc:
     MAJOR    = 1
     MINOR    = 8
-    TINY     = 1
+    TINY     = 2
     
     STRING   = [MAJOR, MINOR, TINY].join('.')
     
-    CODENAME = "Infinite Smoothie".freeze
+    CODENAME = "Ruby Razor".freeze
     
     RACK     = [1, 0].freeze # Rack protocol version
   end
