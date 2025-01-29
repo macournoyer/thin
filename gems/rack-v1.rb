d55a57a30af504bb7afaf97fdb4bbc69baf989a6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-01-30 01:32:54 +1300

    Add rdoc to gemfile.

--- gems/rack-v1.rb
+++ gems/rack-v1.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
-
-gemspec path: "../"
+eval_gemfile "../Gemfile"
 
 gem 'rack', '~> 1.0'
-
-group :development do
-  gem "rake-compiler"
-end
-
-group :test do
-  gem "rake", ">= 12.3.3"
-  gem "rspec", "~> 3.5"
-end
-
--- gems/rack-v2.rb
+++ gems/rack-v2.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
-
-gemspec path: "../"
+eval_gemfile "../Gemfile"
 
 gem 'rack', "~> 2.0"
-
-group :development do
-  gem "rake-compiler"
-end
-
-group :test do
-  gem "rake", ">= 12.3.3"
-  gem "rspec", "~> 3.5"
-end
