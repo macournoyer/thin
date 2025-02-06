f513adb94ccb4e8a65b442dac9e78ccf7fb6ffaa
Andrea Lorenzetti <andrea.lorenzetti@nozominetworks.com>
2025-02-06 13:06:28 +0100

    Support rack 3.x and rackup
    * Split classes Rack/Rackup::Handler::Thin
    * Updated CI to run on rack 1/2/3

--- lib/rackup/handler/thin.rb
+++ lib/rackup/handler/thin.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'rack/handler'
+require 'rackup/handler'
 require_relative '../../thin/rackup/handler'
 
-module Rack
+module Rackup
   module Handler
     class Thin < ::Thin::Rackup::Handler
     end
 
-    register :thin, Thin.to_s
+    register :thin, Thin
   end
 end
